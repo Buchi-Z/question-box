6b340e223a9bc33dce8bdd7d41434122e5b7dcc2
Buchi-Z <d2gyy5nn7f@privaterelay.appleid.com>
2026-06-30 23:09:43 +0900

    add admin panel

---
+++ functions/api/list.js
@@ -0,0 +1,26 @@
+export async function onRequestGet(context) {
+  try {
+    const result = await context.env.DB.prepare(
+      "SELECT * FROM questions ORDER BY created_at DESC"
+    ).all();
+
+    return new Response(JSON.stringify({
+      success: true,
+      data: result.results
+    }), {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+  } catch (err) {
+    return new Response(JSON.stringify({
+      success: false,
+      error: err.message
+    }), {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }
+}
